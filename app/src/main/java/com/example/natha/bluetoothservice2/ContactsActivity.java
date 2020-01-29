6a2acfd147d44c89217225a49317e2716b6e79b7
nathan-pc <nn5g14@soton.ac.uk>
2020-01-29 19:44:21 +0000

    Cleaned Contacts Activity

--- app/src/main/java/com/example/natha/bluetoothservice2/ContactsActivity.java
+++ app/src/main/java/com/example/natha/bluetoothservice2/ContactsActivity.java
@@ -1,36 +1,29 @@
 package com.example.natha.bluetoothservice2;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.media.Image;
 import android.net.Uri;
 import android.os.CountDownTimer;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-
-import static android.Manifest.permission.CALL_PHONE;
 
 public class ContactsActivity extends AppCompatActivity {
 
-    private ImageButton callbutton;
-    ImageButton contactshomebutton;
+    private ImageButton callButton;
+    private ImageButton contactsHomeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        callbutton = (ImageButton) findViewById(R.id.callbutton);
-        contactshomebutton = (ImageButton) findViewById(R.id.contactshomebutton);
+        callButton = findViewById(R.id.callbutton);
+        contactsHomeButton = findViewById(R.id.contactshomebutton);
 
         // add button listener
-        callbutton.setOnClickListener(new View.OnClickListener() {
+        callButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
@@ -39,18 +32,16 @@ public class ContactsActivity extends AppCompatActivity {
                 callIntent.setData(Uri.parse("tel:07534824108"));
                 startActivity(callIntent);
 
-                callbutton.setColorFilter(Color.BLACK);
+                callButton.setColorFilter(Color.BLACK);
                 new CountDownTimer(100, 50) {
 
                     @Override
                     public void onTick(long arg0) {
-                        // TODO Auto-generated method stub
-
                     }
 
                     @Override
                     public void onFinish() {
-                        callbutton.setColorFilter(Color.WHITE);
+                        callButton.setColorFilter(Color.WHITE);
                     }
                 }.start();
 
@@ -61,7 +52,7 @@ public class ContactsActivity extends AppCompatActivity {
     }
 
     public void main1() {
-        contactshomebutton.setColorFilter(Color.BLACK);
+        contactsHomeButton.setColorFilter(Color.BLACK);
         new CountDownTimer(100, 50) {
 
             @Override
@@ -72,11 +63,9 @@ public class ContactsActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                contactshomebutton.setColorFilter(Color.WHITE);
+                contactsHomeButton.setColorFilter(Color.WHITE);
             }
         }.start();
-        /*Intent myintent7 = new Intent(this, MainActivity.class); // new intent of analytics activity
-        startActivity(myintent7); // start activity , switch to graph1*/
         finish();
         overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right); // insert animations
     }
